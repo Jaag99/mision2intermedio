3c4bbb1d097f12f5e80df0bbccfc09b12628d13d
ImGabis <artediverso.lab@gmail.com>
2025-08-27 18:39:40 -0500

    mision 2

---
+++ src/components/Card.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+const card = () => {
+  return (
+    <div>
+      <h1>Misión 2</h1>
+
+      <div>
+        <h2>Modelo entidad relación (MER)</h2>
+        <img
+          src="https://res.cloudinary.com/dvnacfjko/image/upload/v1756264386/WhatsApp_Image_2025-08-26_at_7.48.31_PM_fncbtl.jpg"
+          alt=""
+        />
+        <h2>Insertar registro en tabla cliente </h2>
+        <img
+          src="https://res.cloudinary.com/dvnacfjko/image/upload/v1756264700/WhatsApp_Image_2025-08-26_at_8.10.35_PM_ewl0no.jpg"
+          alt=""
+        />
+        <img
+          src="https://res.cloudinary.com/dvnacfjko/image/upload/v1756265148/WhatsApp_Image_2025-08-26_at_8.21.10_PM_cch52p.jpg"
+          alt=""
+        />
+        <h2>Insertar registro en tabla técnico</h2>
+        <img
+          src="https://res.cloudinary.com/dvnacfjko/image/upload/v1756265340/WhatsApp_Image_2025-08-26_at_8.28.58_PM_e3kfxw.jpg"
+          alt=""
+        />
+        <img
+          src="https://res.cloudinary.com/dvnacfjko/image/upload/v1756265386/WhatsApp_Image_2025-08-26_at_8.30.55_PM_v2cd6o.jpg"
+          alt=""
+        />
+        <h2>Actualización de datos en tabla técnico</h2>
+        <img
+          src="https://res.cloudinary.com/dvnacfjko/image/upload/v1756265508/WhatsApp_Image_2025-08-26_at_8.38.24_PM_b9yuy9.jpg"
+          alt=""
+        />
+        <img
+          src="https://res.cloudinary.com/dvnacfjko/image/upload/v1756265579/WhatsApp_Image_2025-08-26_at_8.38.57_PM_devsc2.jpg"
+          alt=""
+        />
+        <h2>Insertar registro en tabla equipo</h2>
+        <img
+          src="https://res.cloudinary.com/dvnacfjko/image/upload/v1756265734/WhatsApp_Image_2025-08-26_at_8.46.53_PM_eh16b1.jpg"
+          alt=""
+        />
+        <img
+          src="https://res.cloudinary.com/dvnacfjko/image/upload/v1756265851/WhatsApp_Image_2025-08-26_at_8.48.10_PM_ya9ifw.jpg"
+          alt=""
+        />
+        <h2>Actualización de datos en tabla equipo</h2>
+        <img
+          src="https://res.cloudinary.com/dvnacfjko/image/upload/v1756265999/WhatsApp_Image_2025-08-26_at_8.50.51_PM_urhvaj.jpg"
+          alt=""
+        />
+        <img
+          src="https://res.cloudinary.com/dvnacfjko/image/upload/v1756266062/WhatsApp_Image_2025-08-26_at_8.52.04_PM_gfk1mb.jpg"
+          alt=""
+        />
+        <h2>Eliminación de registro tabla cliente</h2>
+        <img
+          src="https://res.cloudinary.com/dvnacfjko/image/upload/v1756266162/WhatsApp_Image_2025-08-26_at_8.54.24_PM_tkukfs.jpg"
+          alt=""
+        />
+        <img
+          src="https://res.cloudinary.com/dvnacfjko/image/upload/v1756266228/WhatsApp_Image_2025-08-26_at_8.55.19_PM_imjlm5.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default card;
